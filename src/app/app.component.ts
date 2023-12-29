a6d055bc04a0abcae4bd1666e15aea45bc8c8d71
flpmartins <filipemartins0303@gmail.com>
2023-12-29 14:46:28 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { catchError, switchMap, finalize } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  countries: any[] = [];
+  filteredCountries: any[] = [];
+  searchText = '';
+  displayLimit = 1;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getCountries()
+      .pipe(
+        switchMap((response) => {
+          if (response) {
+            this.countries = response;
+          }
+          return of(null);
+        }),
+        catchError((error) => {
+          console.error('Error fetching countries:', error);
+          return of(null);
+        }),
+        finalize(() => {
+          this.updateFilteredCountries();
+        })
+      )
+      .subscribe();
+  }
+
+  updateFilteredCountries() {
+    this.filteredCountries = this.countries
+      .filter((country) =>
+        country.name.common.toLowerCase().includes(this.searchText.toLowerCase())
+      )
+      .slice(0, this.displayLimit);
+  }
+
+  onSearchChange() {
+    this.updateFilteredCountries();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiService } from './api.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ApiService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
